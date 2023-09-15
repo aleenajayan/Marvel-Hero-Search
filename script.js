15eedee827839935a6f4f8dbae8be6607fcb1ba6
Aleena Jayan <89581245+aleenajayan@users.noreply.github.com>
2023-09-15 11:07:01 +0530

    UI completed

---
+++ script.js
@@ -0,0 +1,11 @@
+let input1=document.getElementById("chara-input");
+let button =document.getElementById("button1");
+
+function add1(){
+    if(input1.value === ''){
+        alert("You must enter something!")
+     }
+     else{
+        const url ='https://gateway.marvel.com:443/v1/public/characters?apikey=d91bb452a6d0c899d386e05307d66af3'
+     }
+}
